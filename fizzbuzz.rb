a269f22093779c372c53038e047bb91ea72b7806
Virginia Dooley <vcdooley@gmail.com>
2018-09-25 18:32:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,3 @@
-# def fizzbuzz(n) 
-#   1.upto(100) do |i|
-#     puts i
-# end
-
-# fizz_3 = "Fizz" if n % 3 == 0
-# fizz_5 = "Buzz" if n % 5 == 0 
-# fizz_15 = "FizzBuzz" if n % 3 == 0 && n % 5 == 0
-#   end
-# end
 def fizzbuzz(n)
   if n % 5 == 0 && n % 3 == 0
     return "FizzBuzz"
